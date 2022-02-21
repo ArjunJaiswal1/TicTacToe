15709a6fa337e51c8eee7e4325b5e011d7ffb825
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-21 09:53:32 +0530

    [Arjun] Add ; Added Executed

---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIC_TAC_TOE
+{
+    class TicTacToeGame
+    {
+        //declaring 1d array
+        char[] board = new char[10];
+
+        //adding empty spaces to each element
+        public void CreateBoard()
+        {
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
